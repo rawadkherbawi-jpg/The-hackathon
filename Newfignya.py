036c701faae619a5914afdd3ee626575d684d3c9
noamt <noamts09@gmail.com>
2026-08-20 12:02:41 +0300

    rr

--- Newfignya.py
+++ Newfignya.py
@@ -4,21 +4,58 @@ def on_click():
     import webbrowser
     webbrowser.open("https://www.youtube.com/watch?v=Aq5WXmQQooo")
 
+def on_click1():
+    import webbrowser
+    webbrowser.open("https://www.youtube.com/watch?v=ZAqIoDhornk")
+
+def on_click2():
+    import webbrowser
+    webbrowser.open("https://www.youtube.com/watch?v=NybHckSEQBI&pp=ygUMYWxnZWJyYSBtYXRo")
+
+def on_click3():
+    import webbrowser
+    webbrowser.open("https://www.youtube.com/watch?v=r-SCyD7f_zI")
+
+
+def on_click4():
+     import webbrowser
+     webbrowser.open("https://www.youtube.com/watch?v=3tisOnOkwzo")
+
+
 
 def button_click():
-    print(f"You clicked button Math")
-    # root = tk.Tk()
     root.title("Math Sections")
     root.geometry("800x500")
-    button = tk.Button(root, text="Geometry", command=on_click)
-    button.pack(pady=50)
+    button1 = tk.Button(root, text="Geometry", command=on_click)
+    button1.place(x = 390, y= 60)
+    button1 = tk.Button(root, text="Algebra", command=on_click2)
+    button1.place(x=390, y=110)
     root.mainloop()
+
+
+def button_click1():
+    root.title("Physics Sections")
+    root.geometry("800x500")
+    button1 = tk.Button(root, text="Mechanic", command=on_click1)
+    button1.place(x = 295, y = 60)
+    button3 = tk.Button(root, text="Electric", command=on_click3)
+    button3.place(x=295, y=110)
+    root.mainloop()
+
+def science():
+    button = tk.Button(root, text="physics", command=button_click1)
+    button.place(x=300, y=20)
 
 
 root = tk.Tk()
 root.title("Tkinter Button Example")
 root.geometry("800x500")
 button = tk.Button(root, text="math", command=button_click)
-button.pack(pady=20)
+button.place(x = 400, y= 20)
+button = tk.Button(root, text="physics", command=button_click1)
+button.place(x = 300, y =20)
+button = tk.Button(root, text="science", command=science)
+button.place(x = 200, y =20)
+
 
 root.mainloop()
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import tkinter as tk
+import webbrowser
 def on_click():
-    import webbrowser
     webbrowser.open("https://www.youtube.com/watch?v=UhgZBYtDE3Q&list=PL6LUMj1bhfm62DCHDEDK9f2iRSeNVBGb4&index=1")
 root = tk.Tk()
 root.title("Tkinter Button Example")
 root.geometry("800x500")
 button = tk.Button(root, text="biology", command=on_click)
-button.pack(pady=20)
+button.place(x = 300, y = 20)
 button1 = tk.Button(root, text="math", command=on_click)
-button1.pack(pady=200)
-
+button1.place(x = 400, y = 20)
+button2 = tk.Button(root, text="physics", command=on_click)
+button2.place(x = 500, y = 20)
 root.mainloop()
