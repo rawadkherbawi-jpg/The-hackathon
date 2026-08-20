b9b5e299a829390ea247494fd663cecc1f451d48
Rawad <rawadkherbawi@gmail.com>
2026-08-20 10:06:07 +0300

    rawad

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import tkinter as tk
-
 def on_click():
-    print("Button was clicked!")
-
+    import webbrowser
+    webbrowser.open("https://www.youtube.com/watch?v=UhgZBYtDE3Q&list=PL6LUMj1bhfm62DCHDEDK9f2iRSeNVBGb4&index=1")
 root = tk.Tk()
 root.title("Tkinter Button Example")
-root.geometry("300x200")
-
+root.geometry("800x500")
 button = tk.Button(root, text="biology", command=on_click)
-
 button.pack(pady=20)
+button1 = tk.Button(root, text="math", command=on_click)
+button1.pack(pady=200)
+
 root.mainloop()
